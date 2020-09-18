0309afcfb309015932977f4c39d3a22b61db2afe
sleepy81k <nabilnico@gmail.com>
2020-09-18 21:32:19 +0700

    first

---
+++ Soal5.js
@@ -0,0 +1,13 @@
+function santriProgrammer(nama, status, asal) {
+  console.log(nama, asal);
+  status();
+}
+
+function status() {
+    alert ("Status nya adalah Santri")
+  
+}
+
+santriProgrammer ("Nama Santri Rahmad",status,"berasal dari kota medan")
+// console.log("Nama Santri Rahmad","berasal dari kota medan");
+// console ()
